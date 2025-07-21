fdc357dbb6963175406b1ad2267ba8132d5e2dd9
Diksha3001 <dikshadeshmane2004@gmail.com>
2025-07-21 18:02:47 +0530

    Day One

---
+++ src/com/DayTwo/LearnComment2.java
@@ -0,0 +1,11 @@
+package com.DayTwo;
+
+public class LearnComment2 {
+
+    public static void main(String args[]) {
+        // Prints Hello World
+       // System.out.println("Hello World");
+        // Prints GeeksForGeeks
+        System.out.println("GeeksForGeeks");
+    }
+}
---
+++ src/com/DayTwo/PrintHelloWorld.java
@@ -0,0 +1,11 @@
+/*package whatever //do not write package name here */
+package com.DayTwo;
+
+ import java.util.*;
+
+public class PrintHelloWorld {
+	public static void main (String[] args) {
+	System.out.println("Hello World");
+		
+	}
+}
