3c4f87921ccfb55a57d176098831768fddeb2522
Diksha3001 <dikshadeshmane2004@gmail.com>
2025-07-22 22:13:32 +0530

    Day Two

---
+++ src/com/DayThree/ConcatinatInteger.java
@@ -0,0 +1,16 @@
+package com.DayThree;
+import java.util.*;
+
+public class ConcatinatInteger {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        
+        System.out.print(a+""+b);
+        
+    }
+}
+
+
---
+++ src/com/DayThree/Swap.java
@@ -0,0 +1,18 @@
+package com.DayThree;
+import java.util.*;
+public class Swap	
+{
+	    public static void main(String args[]) {
+	        Scanner sc = new Scanner(System.in);
+	        int a = sc.nextInt();
+	        int b = sc.nextInt();
+	        // Write Code to Swap
+	        int temp=a;
+	        a=b;
+	        b=temp;
+
+	        System.out.println(a + " " + b);
+	    }
+	}
+
+
