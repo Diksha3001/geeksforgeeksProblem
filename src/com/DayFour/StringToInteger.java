9e3aa7d877be31f304cc0c79f28e7f7b468b4704
Diksha3001 <dikshadeshmane2004@gmail.com>
2025-07-23 21:06:28 +0530

    Day Four

---
+++ src/com/DayFour/StringToInteger.java
@@ -0,0 +1,14 @@
+package com.DayFour;
+import java.util.*;
+
+public class StringToInteger {
+	public static void main(String args[]) {
+	        Scanner sc = new Scanner(System.in);
+	        String num = sc.nextLine();
+	        // TypeCast to int double it and print
+	        int i=Integer.parseInt(num);
+	        System.out.println(i*2);
+	    }
+	}
+	
+
---
+++ src/com/DayFour/TypeCast.java
@@ -0,0 +1,13 @@
+package com.DayFour;
+import java.util.*;
+public class TypeCast {
+	    public static void main(String args[]) {
+	        Scanner sc = new Scanner(System.in);
+	        double d = sc.nextDouble();
+	        // Typecast and print
+	        int i=(int)d;
+	        System.out.println(i);
+	    }
+}
+
+
