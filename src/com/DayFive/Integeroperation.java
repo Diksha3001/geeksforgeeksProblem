0790fe8ee8a5e4872054aeee999ae034f073b15b
Diksha3001 <dikshadeshmane2004@gmail.com>
2025-07-27 00:07:59 +0530

    Day Five

---
+++ src/com/DayFive/Integeroperation.java
@@ -0,0 +1,17 @@
+package com.DayFive;
+import java.util.*;
+public class Integeroperation {
+
+
+	    public static void main(String args[]) {
+	        // Implement Scanner Class
+	        //  Take input
+	        //  Add 10 and Print
+	        
+	        Scanner sc=new Scanner(System.in);
+	        int n=sc.nextInt();
+	        System.out.println(n+10);
+	    }
+}
+	
+
---
+++ src/com/DayFive/StringDemo1.java
@@ -0,0 +1,18 @@
+package com.DayFive;
+import java.util.*;
+public class StringDemo1 {
+
+	    public static void solve(String s) {
+	       
+	        // Print S and move the cursor to a new line
+	        System.out.println(s);
+	    }
+	    
+	   public static void main(String[] args) {
+		   Scanner sc = new Scanner(System.in);
+	        String s = sc.nextLine();
+	        StringDemo1.solve(s);
+	}
+	}
+	
+
